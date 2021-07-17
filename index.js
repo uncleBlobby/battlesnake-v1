427d69814fe76373923f96554a365fdb03010bfa
Dustin <dc.christianson@gmail.com>
2021-07-17 12:28:36 -0600

    local hosting

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ function handleStart(request, response) {
   //stores request in gamedata object
   let gameData = request.body
 
+  console.log(`START GAME data`);
+  console.log(gameData);
+
+  
+
   //gameData.game = object describing the game being played
   //gameData.turn = integer for current turn number
   //gameData.board = object describing the initial state of the gameboard
@@ -44,19 +49,68 @@ function handleStart(request, response) {
 }
 
 function handleMove(request, response) {
-  let gameData = request.body
+  let gameData = request.body;
+  let move;
+  console.log(`gameData Object`)
   console.log(gameData);
+
+  let me = gameData.you;
+
+  console.log(`my info`);
+  console.log(me);
+  console.log(`my head`);
+  console.log(me.head);
   //gameData.game = object describing the game being played
   //gameData.turn = integer for current turn number
   //gameData.board = object describing the initial state of the gameboard
   //gameData.you = object describing your battlesnake
 
-  let possibleMoves = ['up', 'down', 'left', 'right']
-  let move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
+  console.log(`board object`);
+  console.log(gameData.board);
+
+  //pick a direction, move that direction until you find a wall, then turn any direction EXCEPT the opposite direction
+
+
+  //function to calculate range of available moves around current position
+
+  function calculateAvailableMoves() {
+    let rangeUp = gameData.board.height - me.head.y -1;
+    let rangeDown = me.head.y;
+    let rangeRight = gameData.board.width - me.head.x -1;
+    let rangeLeft = me.head.x;
+
+    console.log(`range up: ${rangeUp}, range down: ${rangeDown}, range Right: ${rangeRight}, range Left: ${rangeLeft}`);
+
+    let ranges = {"up": rangeUp, "down": rangeDown, "right": rangeRight, "left": rangeLeft};
+    return ranges;
+  };
+
+  function makeMove() {
+    let ranges = calculateAvailableMoves();
+    console.log(ranges);
+
+    if(ranges.up > 0){
+      move = 'up';
+    };
+    if(ranges.right > 0){
+      move = 'right';
+    };
+    if(ranges.down > 0){
+      move = 'down';
+    };
+    if(ranges.left > 0){
+      move = 'left';
+    };
+    console.log(`move inside makeMove`);
+    console.log(move);
+    return move;
+  };
+
+  //makeMove();
 
   console.log('MOVE: ' + move)
   response.status(200).send({
-    move: move
+    move: makeMove()
   })
 }
 
